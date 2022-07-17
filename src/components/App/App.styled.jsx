69d43891de204d2cf68cc51e44b4a3c8cbe6de61
Maksimchik36 <Peltek1985@gmail.com>
2022-07-17 23:11:27 +0300

    add styles to components

---
+++ src/components/App/App.styled.jsx
@@ -0,0 +1,42 @@
+import styled from "@emotion/styled";
+
+/*
+ * Стили компонента App
+ */
+// .App {
+//   display: grid;
+//   grid-template-columns: 1fr;
+//   grid-gap: 16px;
+//   padding-bottom: 24px;
+// }
+
+
+// style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+
+
+export const Container = styled.div`
+display: grid;
+grid-template-columns: 1fr;
+grid-gap: 16px;
+padding-bottom: 24px;
+// background-image: linear-gradient(to right, #eea2a2 0%, #bbc1bf 19%, #57c6e1 42%, #b49fda 79%, #7ac5d8 100%);
+`;
+
+// export const TitleMain = styled.h1`
+// margin: 10px;
+// font-size: 40px;
+// text-align: center;
+// `
+
+// export const TitleSecondary = styled.h2`
+// margin: 10px;
+// font-size: 30px;
+// text-align: center;
+// `
---
+++ src/service/image-service.js
@@ -0,0 +1,3 @@
+// const API_KEY = '27623768-405768f09194e046df4a054c4';
+
+// `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
