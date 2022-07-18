630afbe49446ab4cc0d105a3e6b0ca90c483150b
Maksimchik36 <Peltek1985@gmail.com>
2022-07-18 17:27:46 +0300

    add API

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,9 +2,22 @@ import { Component } from "react";
 import { Container } from "./App.styled";
 import Searchbar from "components/Searchbar";
 import ImageGallery from "components/ImageGallery";
+import * as ImageService from '../../service/image-service';
 
 class App extends Component {
+  state = {
+    query: '',
+    images: [],
+    page: 1,
+    isEmpty: false,
+    isVisible: false,
+    isLoading: false,
+    error: null,
+  };
+
   render() {
+    const result = ImageService.getImages("cat");
+    console.log(result);
     return (
       <Container>        
         <Searchbar onSubmit> </Searchbar>
--- src/service/image-service.js
+++ src/service/image-service.js
@@ -1,3 +1,17 @@
-// const API_KEY = '27623768-405768f09194e046df4a054c4';
+import axios from "axios";
+
+const API_KEY = '27623768-405768f09194e046df4a054c4';
+axios.defaults.baseURL = "https://pixabay.com/api/";
+axios.defaults.headers.common['Authorization'] = API_KEY;
+axios.defaults.params = {
+  imageType: 'photo',
+  orientation: 'horizontal',
+  per_page: 12,
+};
+
+export const getImages = async (query, page) => {
+  const {data} = await axios.get(`search?query=${query}&page=${page}`);
+  return data;
+};
 
 // `https://pixabay.com/api/?q=cat&page=1&key=${API_KEY}&image_type=photo&orientation=horizontal&per_page=12`
